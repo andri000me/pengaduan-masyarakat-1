83ad9e0ce5f01f81d380174ca6599a85ae236be3
Andri Firman Saputra <andrifirmansaputra1@gmail.com>
2021-02-10 18:52:40 +0700

    tambah database

---
+++ pengaduan_masyarakat.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 10 Feb 2021 pada 12.51
+-- Versi server: 10.4.17-MariaDB
+-- Versi PHP: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pengaduan_masyarakat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `masyarakat`
+--
+
+CREATE TABLE `masyarakat` (
+  `nik` varchar(16) NOT NULL,
+  `nama` varchar(35) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `telp` varchar(15) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `masyarakat`
+--
+
+INSERT INTO `masyarakat` (`nik`, `nama`, `username`, `password`, `telp`, `alamat`) VALUES
+('21312313123', 'tes2323', 'asd', '$2y$10$1Vd87nOV4PP/7FgK3EE7gOCJw90hrNzD6SGwsHcD//mbdQ1aY1mum', '2132323', 'asdasd2'),
+('3670000000000000', 'tesasd', 'tes123', '$2y$10$sIc2m/nSDBzVYyqGmMaCJONJSUMt3wmhr9CfSDAfAeCS318TNoz8e', '0812345678922', 'tes alamatsad');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengaduan`
+--
+
+CREATE TABLE `pengaduan` (
+  `id_pengaduan` int(11) NOT NULL,
+  `tgl_pengaduan` date NOT NULL,
+  `nik` varchar(16) NOT NULL,
+  `isi_laporan` text NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `status` enum('proses','selesai') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pengaduan`
+--
+
+INSERT INTO `pengaduan` (`id_pengaduan`, `tgl_pengaduan`, `nik`, `isi_laporan`, `foto`, `status`) VALUES
+(1, '2021-02-10', '3670000000000000', 'asd', '7a4a66cc2e2c65d633ffb00b05e8f87d.jpg', 'selesai'),
+(2, '2021-02-10', '3670000000000000', 'asd', '1.PNG', 'selesai'),
+(9, '2021-02-09', '3670000000000000', 'asd', '1.PNG', 'selesai');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `nama_petugas` varchar(35) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `telp` varchar(13) NOT NULL,
+  `level` enum('admin','petugas') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `petugas`
+--
+
+INSERT INTO `petugas` (`id_petugas`, `nama_petugas`, `username`, `password`, `telp`, `level`) VALUES
+(1, 'admin', 'admin', '$2y$10$sIc2m/nSDBzVYyqGmMaCJONJSUMt3wmhr9CfSDAfAeCS318TNoz8e', '0812345678229', 'admin'),
+(3, 'Andri Firman Saputra', 'andri123', '$2y$10$66nugRlHWpboGYjMT9uk8OUvYdrjTYDZuU5.iaVX2ZQSUDDIeuEOi', '087808675313', 'petugas');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tanggapan`
+--
+
+CREATE TABLE `tanggapan` (
+  `id_tanggapan` int(11) NOT NULL,
+  `id_pengaduan` int(11) NOT NULL,
+  `tgl_tanggapan` date NOT NULL,
+  `tanggapan` text NOT NULL,
+  `id_petugas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tanggapan`
+--
+
+INSERT INTO `tanggapan` (`id_tanggapan`, `id_pengaduan`, `tgl_tanggapan`, `tanggapan`, `id_petugas`) VALUES
+(1, 1, '2021-02-10', 'tes', 1),
+(2, 2, '2021-02-10', 'tes', 1),
+(5, 0, '2021-02-09', 'asdasdasdad', 3),
+(6, 0, '2021-02-09', 'asdasdasdasd', 3),
+(7, 0, '2021-02-09', 'asdasd', 3),
+(10, 9, '2021-02-09', 'asdasdasd', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `masyarakat`
+--
+ALTER TABLE `masyarakat`
+  ADD PRIMARY KEY (`nik`);
+
+--
+-- Indeks untuk tabel `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  ADD PRIMARY KEY (`id_pengaduan`),
+  ADD KEY `nik` (`nik`);
+
+--
+-- Indeks untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- Indeks untuk tabel `tanggapan`
+--
+ALTER TABLE `tanggapan`
+  ADD PRIMARY KEY (`id_tanggapan`),
+  ADD KEY `id_pengaduan` (`id_pengaduan`),
+  ADD KEY `id_petugas` (`id_petugas`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  MODIFY `id_pengaduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `tanggapan`
+--
+ALTER TABLE `tanggapan`
+  MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
